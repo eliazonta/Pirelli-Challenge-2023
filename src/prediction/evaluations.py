dd99a2e49e6146e46d4ad7338f0c5ff221673c11
NielsenErik <eriknielsen98@gmail.com>
2023-11-15 10:39:12 +0100

    update models

--- src/prediction/evaluations.py
+++ src/prediction/evaluations.py
@@ -1,8 +1,10 @@
 from sklearn.model_selection import train_test_split
 import sklearn.preprocessing as preprocessing
-from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression, Perceptron, TheilSenRegressor
+from sklearn.naive_bayes import GaussianNB
 import sklearn.decomposition as decomposition
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, confusion_matrix, classification_report
+from sklearn.neural_network import MLPRegressor
 from sklearn.svm import SVR, LinearSVR
 from sklearn.ensemble import GradientBoostingRegressor
 import os
@@ -32,6 +34,26 @@ def lin_regr(x, y):
 def grad_boost(x, y):
     print_configs("Model: Gradient Boosting")
     model = GradientBoostingRegressor(random_state=0).fit(x, y)
+    return model
+
+def perceptron(x, y):
+    print_debugging("Model: Perceptron")
+    model = Perceptron(tol=1e-3, random_state=0).fit(x, y)
+    return model
+
+def naive_bayes(x, y):
+    print_debugging("Model: Naive Bayes")
+    model = GaussianNB().fit(x, y)
+    return model
+
+def theilsen(x, y):
+    print_debugging("Model: TheilSen")
+    model = TheilSenRegressor(random_state=0).fit(x, y)
+    return model
+
+def ml_nn(x, y):
+    print_configs("Model: Multi Layer Perceptron")
+    model = MLPRegressor(random_state=1, max_iter=2000, activation='tanh', solver='lbfgs').fit(x, y)
     return model
 
 def nn_linear(x, y):
@@ -89,6 +111,40 @@ def evaluations(x,y, f, desc = None):
     
     return model
 
+def evaluations_on_new_data(x,y, f, desc = None):
+    df_test, _ = get_data(80)
+    df_test = df_test.fillna(0)
+    df_test.drop(columns=['y-m-day-hour_3_rounded'], inplace=True)
+    y_test = df_test['cumulative_per_day_CYCLE_ABORTED_day'].to_numpy()
+    x_test , _= linearize_input(df_test)
+    
+    X_test, y_test, scaler = scale(x_test, y_test)
+    
+    if desc is not None:
+        print_info(desc)
+    
+    X_train = x
+    y_train = y
+    model = f(X_train, y_train)
+    train_score = model.score(X_train, y_train)
+    print_info(f"Model score: {train_score}")
+    model_prediction = model.predict(X_test)
+    accuracy_score = model.score(X_test, y_test)
+    mse = mean_squared_error(y_test, model_prediction)
+    r2 = r2_score(y_test, model_prediction)
+    print_info("Metrics are:\n Accuracy: {}\n MSE: {} \n R2: {}\n\n".format(accuracy_score, mse, r2))
+    
+    print_info("Predictions:")
+    for i in range(len(y_test)//20):
+        print("y_test[{}]: {}, model_prediction[{}]: {}".format(i, y_test[i], i, model_prediction[i]))
+    
+    return model
+    
+    
+    
+    
+    
+
 def linearize_input(df, side = None):
     list_status = ['CYCLE_COMPLETED', 'CYCLE_ABORTED', 'CYCLE_NOT_STARTED', 'CYCLE_COMPLETED_L', 'CYCLE_ABORTED_L', 'CYCLE_NOT_STARTED_L',
                    'CYCLE_COMPLETED_R', 'CYCLE_ABORTED_R', 'CYCLE_NOT_STARTED_R', 'cumulative_per_day_CYCLE_COMPLETED_day', 
@@ -117,10 +173,11 @@ def get_data(mach_index):
 
 if __name__ == '__main__':
     train_df = []
-    for i in range(20):
-        df, current_machine = get_data(40)
+    for i in range(5):
+        df, current_machine = get_data(i)
         train_df.append(df)
     df = pd.concat(train_df)
+    df = df.fillna(0)
     df.drop(columns=['y-m-day-hour_3_rounded'], inplace=True)
     x, x_df = linearize_input(df)
     y = df['cumulative_per_day_CYCLE_ABORTED_day'].to_numpy()
@@ -134,14 +191,23 @@ if __name__ == '__main__':
     model_svr = evaluations(scale_x,scale_y, svr_regression, desc = "SVR")
     model_lin = evaluations(scale_x,scale_y, lin_regr, desc = "Linear Regression")
     model_gb = evaluations(scale_x,scale_y, grad_boost, desc = "Gradient Boosting")
+    model_perceptron = evaluations(scale_x,scale_y, perceptron, desc = "Perceptron")
+    model_naive_bayes = evaluations(scale_x,scale_y, naive_bayes, desc = "Naive Bayes")
+    model_ml_nn = evaluations(scale_x,scale_y, ml_nn, desc = "Multi Layer Perceptron")
+    
+    
+    # model_gb_ = evaluations_on_new_data(scale_x,scale_y, grad_boost, desc = "Gradient Boosting")
     
     figure = plt.subplots(10,3)
     ax1 = plt.subplot(3,1,1)
     ax2 = plt.subplot(3,1,2)
     ax3 = plt.subplot(3,1,3)
     
+    
+    
     ax1.plot(scale_y, label='Actual')
-    ax1.plot(model_svr.predict(scale_x), label='SVR')
+    
+    ax1.plot(model_gb.predict(scale_x), label='Gradient Boosting')
     ax1.legend()
     ax1.grid()
     ax1.set_xlim(0,300)
@@ -151,7 +217,7 @@ if __name__ == '__main__':
     ax2.grid()
     ax2.set_xlim(0,300)
     ax3.plot(scale_y, label='Actual')
-    ax3.plot(model_gb.predict(scale_x), label='Gradient Boosting')
+    ax3.plot(model_ml_nn.predict(scale_x), label='NN')
     ax3.legend()
     ax3.grid()
     ax3.set_xlim(0,300)
